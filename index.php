18178306068b33de6f686c758d06f7bcd1fb6e17
Sergei Yurchenko <sergyurch@gmail.com>
2022-02-20 12:14:13 +0200

    change db settings

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 	// mb_internal_encoding('utf-8');
+	error_reporting(E_ALL);
 	session_start();
 
 	//No authentification or clicked logout
@@ -10,7 +11,7 @@
 		$users_id = implode(',', $_GET['users_id']);
 	}
 
-	$dbh = new PDO('mysql: host=us-cdbr-east-05.cleardb.net; dbname=heroku_cb4f6467da51eac; charset=utf8', 'b7ecca645b3490', 'ec42b495');
+	$dbh = new PDO('mysql: host=localhost; dbname=heroku_cb4f6467da51eac; charset=utf8', 'b7ecca645b3490', 'ec42b495');
 
 	//Delete action
 	if ( isset($_GET['delete']) ) {
